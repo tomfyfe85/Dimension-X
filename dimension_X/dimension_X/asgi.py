4e031084fadddf9062ef4d4579343630f4a36d7a
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-07 21:59:43 +0000

    set up psql and migrated tables

--- dimension_X/dimension_X/asgi.py
+++ dimension_X/dimension_X/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for chitter project.
+ASGI config for dimension_X project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chitter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dimension_X.settings')
 
 application = get_asgi_application()
--- dimension_X/dimension_X/wsgi.py
+++ dimension_X/dimension_X/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for chitter project.
+WSGI config for dimension_X project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chitter.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dimension_X.settings')
 
 application = get_wsgi_application()
