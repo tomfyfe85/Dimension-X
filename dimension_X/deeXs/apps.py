db666f0f45c891a23f9b47ff0092f61bc7c23beb
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-10 19:34:25 +0000

    changed app name from dimeX to deeXs

--- dimension_X/deeXs/apps.py
+++ dimension_X/deeXs/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DimexConfig(AppConfig):
+class DeeXsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dimeX'
+    name = 'deeXs'
