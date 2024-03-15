5e54fa061fa30e1730f0b704b8556ce99bd7f968
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-15 10:06:23 +0000

    refactored test_deeX_model()

--- dimension_X/deeXs/test_.py
+++ dimension_X/deeXs/test_.py
@@ -5,6 +5,5 @@ from .models import DeeX
 
 @pytest.mark.django_db
 def test_deeX_model():
-    deeX = DeeX.objects.create(full_name="Test Name")
-    name = DeeX.objects.all()[0].full_name
-    assert name == "Test Name"
+    DeeX.objects.create(full_name="Test Name")
+    assert DeeX.objects.all()[0].full_name == "Test Name"
