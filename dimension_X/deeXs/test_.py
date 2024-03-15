c549324b787ed115553cd25073c0d5a02f0a03b2
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-15 18:11:53 +0000

    need to figure out way of testing date time - MOCK?

--- dimension_X/deeXs/test_.py
+++ dimension_X/deeXs/test_.py
@@ -1,9 +1,35 @@
 import pytest
+from datetime import datetime
 
 from .models import DeeX
 
 
 @pytest.mark.django_db
-def test_deeX_model():
-    DeeX.objects.create(full_name="Test Name")
-    assert DeeX.objects.all()[0].full_name == "Test Name"
+def test_DeeX_model():
+    date_time = datetime.now()
+    DeeX.objects.create(
+        full_name="Test name",
+        user_name="Test user name",
+        password="TESTing1234",
+        email="some_email@geemail.com",
+        deeX="HI this is a DEEX",
+        slug="/1",
+        time_date=date_time,
+    )
+
+    print(DeeX.objects.all()[0].time_date)
+    assert DeeX.objects.all()[0].full_name == "Test name"
+    assert DeeX.objects.all()[0].user_name == "Test user name"
+    assert DeeX.objects.all()[0].password == "TESTing1234"
+    assert DeeX.objects.all()[0].email == "some_email@geemail.com"
+    assert DeeX.objects.all()[0].deeX == "HI this is a DEEX"
+    assert DeeX.objects.all()[0].slug == "/1"
+    assert DeeX.objects.all()[0].time_date == date_time
+
+    # full_name = models.CharField(max_length=50)
+    # user_name = models.CharField(max_length=50)
+    # password = models.CharField(max_length=50)
+    # email = models.EmailField(max_length=100)
+    # deeX = models.TextField(max_length=140)
+    # slug = models.SlugField()
+    # time_date = models.DateTimeField(auto_now_add=True)
