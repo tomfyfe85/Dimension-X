96244347434e9bf940acd5e5fe32f2d980364107
Tom Fyfe <tomfyfe85@gmail.com>
2024-03-21 20:56:58 +0000

    test_deeX_view passes using reverse().

--- dimension_X/deeXs/tests/test_templates.py
+++ dimension_X/deeXs/tests/test_templates.py
@@ -1,4 +1,4 @@
 def test_deeX_template(client):
-    response = client.get("/deeXs/")
+    response = client.get("")
     assert response.status_code == 200
     assert "DEEX" in response.content.decode("utf_8")
--- dimension_X/deeXs/urls.py
+++ dimension_X/deeXs/urls.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.deeXs)]
+urlpatterns = [path("", views.deeXs, name="deeXs")]
